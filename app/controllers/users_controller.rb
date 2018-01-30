8fcfa92a8fff2a9a47d2cb60439cd1963139cbfd
Rina Hong <rhong1006@gmail.com>
2018-01-30 10:13:04 -0800

    Redirect_to trips_path when user sign up, refactor trip_ctrl to assign user to trip when created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,15 +25,10 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      redirect_to trips_path
+    else
+      render :new
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :reviews, dependent: :destroy
+  has_many :trips, dependent: :destroy
   has_secure_password
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
