09c8bf09c0984052515fe1af45a28b585884bf5c
Rina Hong <rhong1006@gmail.com>
2018-01-29 19:02:11 -0800

    Refactor User model, controller, and view for password_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  has_secure_password
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: VALID_EMAIL_REGEX
+  validates :username, presence: true, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   get('/', { to: 'welcome#index', as: :home })
-  get('/', { to: 'welcome#about', as: :about })
+  get('/about', { to: 'welcome#about', as: :about })
 
   resources :users
   resource :sessions
