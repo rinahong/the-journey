5b9eac5d690ee8dca61669f25ea5878c0d03f1fe
Rina Hong <rhong1006@gmail.com>
2018-01-29 19:03:27 -0800

    Add authenticate, and current_user function in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def user_signed_in?
+    if session[:user_id].present? && current_user.nil?
+      session[:user_id] = nil
+    end
+     session[:user_id].present?
+  end
+  helper_method :user_signed_in?
+
+  def current_user
+    @current_user ||=User.find_by(id: session[:user_id])
+  end
+
+  helper_method :current_user
+
+  private
+  def authenticate_user!
+    unless user_signed_in?
+      redirect_to new_sessions_path, alert: 'You must sign in or sign up first!'
+    end
+  end
 end
