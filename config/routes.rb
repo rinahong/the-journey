e7333aff4c9c3114becfc0bbb69a5a252b2d37e4
Rina Hong <rhong1006@gmail.com>
2018-01-29 18:57:30 -0800

    Add Navs bar and refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  resources :trips
+
+  get('/', { to: 'welcome#index', as: :home })
+  get('/', { to: 'welcome#about', as: :about })
+
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :sessions
+
+  resources :trips
+
+
 end
