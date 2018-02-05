448b4b607d33210e31d4141c3650471393fbffc6
Rina Hong <rhong1006@gmail.com>
2018-02-04 16:22:52 -0800

    Add tag_list in seed. Welcome page display most_used tag's trip

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,16 @@
 class WelcomeController < ApplicationController
   def index
+    tags = ActsAsTaggableOn::Tag.most_used(5)
+    @trips = []
+    tags.each do |tag|
+      tag.taggings.each do |tagging|
+        trip = Trip.find tagging.taggable_id
+        unless @trips.include? trip
+          @trips.push(trip)
+        end
+
+      end
+    end
   end
 
   def about
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,8 @@
-postal_codes_van = ["V6B 0A6", "V6B 0A7", "V6B 0B2", "V6B 0B4", "V6B 0B5", "V6B 0B7", "V6B 0B8", "V6B 0B9", "V6B 0C1", "V6B 0C2", "V6B 0C3", "V6B 0C5", "V6B 0C7", "V6B 0C8", "V6B 0E1", "V6B 0E2", "V6B 0E3", "V6B 0E4", "V6B 0E5", "V6B 0E7", "V6B 0E8", "V6B 0H1", "V6B 0J0", "V6B 0J1", "V6B 0J8", "V6B 0L6", "V6B 0M2", "V6B 0M3", "V6B 0M9", "V6B 1A1", "V6B 1A4", "V6B 1A5", "V6B 1A6", "V6B 1A7", "V6B 1A8", "V6B 1A9", "V6B 1B1", "V6B 1B2", "V6B 1B4", "V6B 1B5", "V6B 1B6", "V6B 1B8", "V6B 1C3", "V6B 1C5", "V6B 1C6", "V6B 1C7", "V6B 1C8", "V6B 1E2", "V6B 1E5", "V6B 1E6", "V6B 1E7", "V6B 1E8", "V6B 1G1", "V6B 1G2", "V6B 1G3", "V6B 1G4", "V6B 1G6", "V6B 1G8", "V6B 1G9", "V6B 1H4", "V6B 1H5", "V6B 1H6", "V6B 1H7", "V6B 1H8", "V6B 1J4", "V6B 1J6", "V6B 1K2", "V6B 1K3", "V6B 1K6", "V6B 1K8", "V6B 1L1", "V6B 1L4", "V6B 1L5", "V6B 1L6", "V6B 1L7", "V6B 1L8", "V6B 1M8", "V6B 1M9", "V6B 1N2", "V6B 1N3", "V6B 1P1", "V6B 1P2", "V6B 1P8", "V6B 1R4", "V6B 1R6", "V6B 1S1", "V6B 1S2", "V6B 1S3", "V6B 1S4", "V6B 1S5", "V6B 1T1", "V6B 1T5", "V6B 1T6", "V6B 1T8", "V6B 1T9", "V6B 1V2", "V6B 1V3", "V6B 1V4", "V6B 1V5", "V6B 1V6", "V6B 1V9", "V6B 1W1", "V6B 1W2", "V6B 1W6", "V6B 1W8", "V6B 1X2", "V6B 1X3", "V6B 1X4", "V6B 1X6", "V6B 1X9", "V6B 1Y1", "V6B 1Y2", "V6B 1Y7", "V6B 1Y9", "V6B 1Z1", "V6B 1Z5", "V6B 1Z6", "V6B 2A7", "V6B 2A8", "V6B 2B2", "V6B 2B3", "V6B 2B5", "V6B 2B7", "V6B 2B9", "V6B 2C8", "V6B 2E2", "V6B 2E6", "V6B 2E8", "V6B 2G2", "V6B 2G4", "V6B 2G5", "V6B 2G6", "V6B 2G9", "V6B 2H3", "V6B 2J2", "V6B 2J3", "V6B 2J7", "V6B 2J8", "V6B 2K4", "V6B 2K7", "V6B 2K8", "V6B 2K9", "V6B 2L1", "V6B 2L2", "V6B 2L3", "V6B 2M1", "V6B 2M4", "V6B 2M6", "V6B 2M8", "V6B 2M9", "V6B 2N4", "V6B 2N5", "V6B 2N7", "V6B 2P2", "V6B 2P3", "V6B 2P4", "V6B 2P6", "V6B 2R1", "V6B 2R5", "V6B 2R7", "V6B 2R9", "V6B 2S1", "V6B 2T2", "V6B 2T3", "V6B 2T5", "V6B 2V2", "V6B 2V3", "V6B 2V5", "V6B 2W2", "V6B 2W5", "V6B 2W6", "V6B 2W7", "V6B 2X1", "V6B 2X5", "V6B 2Y1", "V6B 2Y4", "V6B 2Y5", "V6B 2Y9", "V6B 2Z3", "V6B 2Z4", "V6B 2Z5", "V6B 3A0", "V6B 3A3", "V6B 3A4", "V6B 3A6", "V6B 3A7", "V6B 3B4", "V6B 3B5", "V6B 3B6", "V6B 3B7", "V6B 3C1", "V6B 3E1", "V6B 3E3", "V6B 3E4", "V6B 3E6", "V6B 3E9", "V6B 3G3", "V6B 3G7", "V6B 3H1", "V6B 3H4", "V6B 3H6", "V6B 3H7", "V6B 3J5", "V6B 3J6", "V6B 3K1", "V6B 3K4", "V6B 3K6", "V6B 3K9", "V6B 3L6", "V6B 3L9", "V6B 3M1", "V6B 3M6", "V6B 3N3", "V6B 3N6", "V6B 3N9", "V6B 3P8", "V6B 3R6", "V6B 3R9", "V6B 3S3", "V6B 3V3", "V6B 3V7", "V6B 3W2", "V6B 3W5", "V6B 3W7", "V6B 3W8", "V6B 3X2", "V6B 3X4", "V6B 3X5", "V6B 3X6", "V6B 3X7", "V6B 3Y3", "V6B 3Y4", "V6B 3Y6", "V6B 3Y8", "V6B 3Z1", "V6B 3Z3", "V6B 3Z4", "V6B 3Z5", "V6B 3Z6", "V6B 3Z7", "V6B 3Z8", "V6B 4A1", "V6B 4A4", "V6B 4A6", "V6B 4A9", "V6B 4B3", "V6B 4B8", "V6B 4E2", "V6B 4J2", "V6B 4K2", "V6B 4K8", "V6B 4L2", "V6B 4M3", "V6B 4N2", "V6B 4N4", "V6B 4N5", "V6B 4N6", "V6B 4N7", "V6B 4N8", "V6B 4N9", "V6B 4P4", "V6B 4P7", "V6B 4R3", "V6B 4R8", "V6B 4T5", "V6B 4W4", "V6B 4X2", "V6B 4X7", "V6B 4Y8", "V6B 4Z9", "V6B 5A6", "V6B 5B2", "V6B 5B3", "V6B 5B4", "V6B 5C6", "V6B 5C7", "V6B 5E4", "V6B 5E7", "V6B 5G1", "V6B 5H6", "V6B 5J3", "V6B 5K3", "V6B 5K5", "V6B 5L1", "V6B 5L7", "V6B 5L8", "V6B 5M5", "V6B 5M9", "V6B 5N3", "V6B 5P2", "V6B 5P9", "V6B 5R1", "V6B 5R3", "V6B 5R4", "V6B 5S2", "V6B 5S4", "V6B 5S8", "V6B 5S9", "V6B 5T1", "V6B 5T4", "V6B 5T5", "V6B 5T6", "V6B 5T7", "V6B 5W3", "V6B 5W4", "V6B 5W7", "V6B 5X5", "V6B 5X6", "V6B 5X8", "V6B 5Z3", "V6B 5Z4", "V6B 5Z5", "V6B 5Z6", "V6B 6A1", "V6B 6A2", "V6B 6A3", "V6B 6A7", "V6B 6A8", "V6B 6A9", "V6B 6B1", "V6B 6B3", "V6B 6B4", "V6B 6B6", "V6B 6B7", "V6B 6B8", "V6B 6B9", "V6B 6C1", "V6B 6E1", "V6B 6E3", "V6B 6E4", "V6B 6G1", "V6B 6G3", "V6B 6G8", "V6B 6H4", "V6B 6H5", "V6B 6J3", "V6B 6K5", "V6B 6K6", "V6B 6L2", "V6B 6L5", "V6B 6L6", "V6B 6L9", "V6B 6M5", "V6B 6M6", "V6B 6M9", "V6B 6N6", "V6B 6N7", "V6B 6N9", "V6B 6P6", "V6B 6V1", "V6B 7B6", "V6B 8N9", "V6B 8P5", "V6B 8P6", "V6B 8R2", "V6B 8R3"]
-
 
 Route.destroy_all
+ExpenseTracker.destroy_all
 Trip.destroy_all
 User.destroy_all
-# Tag.destroy_all
-# Tagging.destroy_all
 
 PASSWORD = '123'
 
@@ -35,7 +32,14 @@ end
 users = User.all
 puts Cowsay.say("Create #{users.count} users", :tux)
 
+tags = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+  "n", "o", "p", "q", "r", "x", "t", "u", "v", "w", "x", "y", "z"]
+
 users.each do |user|
+  tags_arr = []
+  rand(0..4).times.each do
+    tags_arr.push(tags.sample)
+  end
   rand(1..3).times.each do
     s_d = DateTime.new(2018,2,01) + rand(1...50).days
     e_d = s_d + rand(1...10).days
@@ -44,7 +48,8 @@ users.each do |user|
       note: Faker::HarryPotter.quote,
       start_date: s_d,
       end_date: e_d,
-      user_id: user.id
+      user_id: user.id,
+      tag_list: tags_arr
     )
   end
 end
