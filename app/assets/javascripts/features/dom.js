74aee3124631cfa393ba7bc54f855fff6a23b4b0
Rina Hong <rhong1006@gmail.com>
2018-02-06 23:35:40 -0800

    Refactor Route Ajax for better coding

--- app/assets/javascripts/features/dom.js
+++ app/assets/javascripts/features/dom.js
@@ -11,64 +11,62 @@ function H (tagName, htmlAttrs = {}, ...elements) {
 }
 
 const Route = {
-  
-}
-
-function createRoute(address, latitude, longitude) {
-  const tripid = $('#map').data('tripid');
-  const myUrl = `http://localhost:3000/trips/${tripid}/routes`;
-  // console.log("lat in create========:", latitude)
-  // console.log("lng in create========:", longitude)
-  const dataToSend = {
-    address: address,
-    latitude: latitude,
-    longitude: longitude
-  }
-  console.log("!!!!!!", dataToSend)
-
-  fetch(
-    myUrl,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(dataToSend)
+  create(address, latitude, longitude) {
+    const tripid = $('#map').data('tripid');
+    const myUrl = `http://localhost:3000/trips/${tripid}/routes`;
+    // console.log("lat in create========:", latitude)
+    // console.log("lng in create========:", longitude)
+    const dataToSend = {
+      address: address,
+      latitude: latitude,
+      longitude: longitude
     }
-  )
-  .then(res => {
-    return res.json()
-  })
-  .then(data => {
-    console.log("res >>> ", data)
-  })
-}
+    console.log("!!!!!!", dataToSend)
 
-function allRoutes() {
-  const tripid = $('#map').data('tripid');
-  const myUrl = `http://localhost:3000/trips/${tripid}/routes`;
-  return new Promise((resolve, reject) => {
-    resolve(fetch(myUrl)
-     .then(res => res.json())
-    )
-  })
-}
-
-function deleteRoute(routeId) {
-  const myUrl = `http://localhost:3000/routes/${routeId}`;
-  return new Promise((resolve, reject) => {
-    resolve(fetch(
-       myUrl,
+    fetch(
+      myUrl,
       {
-        method: 'DELETE'
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dataToSend)
       }
     )
-     .then(res => res.json())
-    )
-  })
+    .then(res => {
+      return res.json()
+    })
+    .then(data => {
+      console.log("res >>> ", data)
+    })
+  },
+
+  all() {
+    const tripid = $('#map').data('tripid');
+    const myUrl = `http://localhost:3000/trips/${tripid}/routes`;
+    return new Promise((resolve, reject) => {
+      resolve(fetch(myUrl)
+       .then(res => res.json())
+      )
+    })
+  },
+
+  delete(routeId) {
+    const myUrl = `http://localhost:3000/routes/${routeId}`;
+    return new Promise((resolve, reject) => {
+      resolve(fetch(
+         myUrl,
+        {
+          method: 'DELETE'
+        }
+      )
+       .then(res => res.json())
+      )
+    })
+  }
 }
 
-allRoutes().then(allRoutes => {
+Route.all().then(allRoutes => {
   allRoutes.map(route =>
     $('#sortable').append(
       `<li class="single-route"><span>${route.address}</span>` +
@@ -87,7 +85,7 @@ $('#map').on('click', '.fa.fa-plus-circle', e => {
   console.log("lat========:", lat)
   console.log("lng========:", lng)
   $('#sortable').append($(`<li class="single-route"><p>${address}</p></li>`));
-  createRoute(address, lat, lng);
+  Route.create(address, lat, lng);
 
   let form = $("#addForm").html();
   $("li.single-route:last-child").append(form);
@@ -100,12 +98,12 @@ $('#sortable').on('click','.fa.fa-minus-square', e => {
   //!!!!!!!!Map also needs to be reload to update the data.
   // !!!!!!!!!I don't like to do this below line... I think using ajax is fancy
   // $(e.target).parent().remove();
-  // deleteRoute(routeId)
+  // Route.delete(routeId)
 
   //Below is alt way but still not working.....
-  deleteRoute(routeId)
+  Route.delete(routeId)
   .then($('#sortable').empty())
-  .then(allRoutes().then( allRoutes => {
+  .then(Route.all().then( allRoutes => {
     allRoutes.map(route =>
       $('#sortable').append(
         `<li class="single-route"><span>${route.address}</span>` +
