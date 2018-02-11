dc6c2a498fbda3f25f26d39ee9f8f885cc9ad8b1
Rina Hong <rhong1006@gmail.com>
2018-02-11 15:14:52 -0800

    Add font awesome icon and refactor css

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -13,9 +13,11 @@ class LikesController < ApplicationController
   end
 
   def destroy
+    trip= Trip.find params[:trip_id]
     like = Like.find params[:id]
     if can? :destroy, like
       like.destroy
+      trip.update(like_count: trip.like_count - 1)
       redirect_to trip_path(like.trip)
     else
       head :unauthorized
