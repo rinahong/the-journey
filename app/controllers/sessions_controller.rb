6610e36ae3e679c6f92f6e04a399a5289907d064
Rina Hong <rhong1006@gmail.com>
2018-02-19 16:32:13 -0800

    Clean up session, welcome ctrls

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,9 @@
 class SessionsController < ApplicationController
   def new
-
   end
 
   def create
    user = User.find_by(email: session_params[:email])
-
    if user && user.authenticate(session_params[:password])
      session[:user_id] = user.id
 
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,12 +2,12 @@ class WelcomeController < ApplicationController
   layout "home_application"
   def index
     tags = ActsAsTaggableOn::Tag.most_used(3)
-    @trips = []
+    @trip_tags_most_used = []
     tags.each do |tag|
       tag.taggings.each do |tagging|
         trip = Trip.find tagging.taggable_id
-        unless @trips.include? trip
-          @trips.push(trip)
+        unless @trip_tags_most_used.include? trip
+          @trip_tags_most_used.push(trip)
         end
       end
     end
