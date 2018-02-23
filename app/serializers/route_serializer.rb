d72e7284ebb1f22e28f110344930a0f2d9e645e8
Rina Hong <rhong1006@gmail.com>
2018-02-22 20:30:09 -0800

    Hard code heroku url into route serializer

--- app/serializers/route_serializer.rb
+++ app/serializers/route_serializer.rb
@@ -9,6 +9,6 @@ class RouteSerializer < ActiveModel::Serializer
   def url
     baseUrl = env === 'development' ? 'http://localhost:3000/' : 'https://awesome-journey.herokuapp.com/'
     # "http://localhost:3000" + route_path(object.id)
-    baseUrl + route_path(object.id)
+    'https://awesome-journey.herokuapp.com/' + route_path(object.id)
   end
 end
