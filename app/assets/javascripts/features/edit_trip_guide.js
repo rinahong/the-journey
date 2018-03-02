f90dfa8fdf7c75a1fa1cea49f7ea7284b71f25ad
Rina Hong <rhong1006@gmail.com>
2018-03-01 20:07:11 -0800

    Hopscotch feature is working

--- app/assets/javascripts/features/edit_trip_guide.js
+++ app/assets/javascripts/features/edit_trip_guide.js
@@ -1,43 +1,43 @@
-{
+
+var tour = {
      id: "hello-hopscotch",
      steps: [
        {
-         target: "title",
+         target: "trip-edit-title",
          title: "Welcome to Trip's edit page!",
          content: "Hey there! You can add, delete, update itineraries on this page!",
          placement: "bottom",
-         xOffset: 'right',
+         xOffset: 'center',
          arrowOffset: 'center'
        },
-       // {
-       //   target: document.querySelectorAll("#general-use-desc code")[1],
-       //   title: "Where to begin",
-       //   content: "At the very least, you'll need to include these two files in your project to get started.",
-       //   placement: "top",
-       //   yOffset: -20
-       // },
-       // {
-       //   target: "map",
-       //   placement: "top",
-       //   title: "Define and start your tour",
-       //   content: "Once you have Hopscotch on your page, you're ready to start making your tour! The biggest part of your tour definition will probably be the tour steps."
-       // },
-       // {
-       //   target: "routes",
-       //   placement: "left",
-       //   title: "Starting your tour",
-       //   content: "After you've created your tour, pass it in to the startTour() method to start it.",
-       //   // yOffset: -25
-       // },
-       // {
-       //   target: "basic-options",
-       //   placement: "left",
-       //   title: "Basic step options",
-       //   content: "These are the most basic step options: <b>target</b>, <b>placement</b>, <b>title</b>, and <b>content</b>. For some steps, they may be all you need.",
-       //   arrowOffset: 100,
-       //   yOffset: -80
-       // }
+       {
+         target: "map",
+         placement: "top",
+         xOffset: 'center',
+         title: "Add a new itinerary",
+         content: "Click on the map!"
+       },
+       {
+         target: "routes",
+         placement: "top",
+         title: "Your Route",
+         content: "Try update duration, delete route, or re-order them by drag and drop!",
+       },
+       {
+         target: "routes",
+         placement: "left",
+         title: "Show itinerary detail",
+         content: "Click the itinerary address in blue"
+       }
      ],
      showPrevButton: true,
      scrollTopMargin: 100
    }
+
+// Start the tour!
+$(document).ready(() => {
+  $('#startTourBtn').on('click', () => {
+    console.log("helloooo")
+    hopscotch.startTour(tour);
+  })
+})
